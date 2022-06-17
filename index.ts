a35677b87948d15d4f6fa1cde164748f5c3c5dc8
Andrei Ihnatsiuk <andrei_ihnatsiuk@epam.com>
2022-06-17 14:37:52 +0300

    crud-api: updated server

--- index.ts
+++ index.ts
@@ -1,45 +1,127 @@
 import { createServer } from 'http';
 import { env } from 'process';
 import { config } from 'dotenv';
+import Storage from './src/storage/storage';
+import { IUser, INewUser } from './src/types/types';
+import { validate as isValidUUID } from 'uuid';
+import { dataChecker } from './src/helpers/helpers';
 
 config();
 
 const PORT = env.PORT || 3000;
 
 const server = createServer(async (request, response) => {
-  if (request.url === '/api/users' && request.method === 'GET') {
-    response.writeHead(200, { 'Content-Type': 'application/json' });
-    response.write('all users records');
-    response.end();
-  }
+  try{
+    const urlArr = request.url?.split('/');
 
-  if (request.url === '/api/users/${userId}' && request.method === 'GET') {
-    response.writeHead(200, { 'Content-Type': 'application/json' });
-    response.write('user with ${userId}');
-    response.end();
-  }
+    if(urlArr && urlArr.length > 3) {
+      const id = urlArr.pop();
+      if(id && !isValidUUID(id)) {
+        response.writeHead(400, { 'Content-Type': 'application/json' });
+        response.write('Request contain INVALID id');
+        response.end();
+      } else if (id && request.method === 'GET') {
+        const user = await Storage.apiGetUserById(id).then((user: IUser | undefined) => user);
 
-  if (request.url === '/api/users' && request.method === 'POST') {
-    response.writeHead(201, { 'Content-Type': 'application/json' });
-    response.write('newly created record of user');
-    response.end();
-  }
+        if(user) {
+          response.writeHead(200, { 'Content-Type': 'application/json' });
+          response.write(JSON.stringify(user));
+          response.end();
+        } else {
+          response.writeHead(404, { 'Content-Type': 'application/json' });
+          response.write(`Can't find user with id: ${id}`);
+          response.end();
+        }
+      } else if(id && request.method === 'PUT') {
+          let data = '';
+        
+          request.on('data', chunk => {
+            data += chunk;
+          });
+          request.on('end', async () => {
+            const params = JSON.parse(data);
 
-  if (request.url === '/api/users/${userId}' && request.method === 'PUT') {
-    response.writeHead(200, { 'Content-Type': 'application/json' });
-    response.write('updated record of user');
-    response.end();
-  }
+            const updatedUserData: INewUser = {
+              username: params.username,
+              age: params.age,
+              hobbies: params.hobbies
+            };
 
-  if (request.url === '/api/users/${userId}' && request.method === 'DELETE') {
-    response.writeHead(200, { 'Content-Type': 'application/json' });
-    response.write('record of user is found and deleted');
-    response.end();
-  }
+            const updatedUser = await Storage.apiUpdateUser(id, updatedUserData).then((user: IUser | undefined) => user)
+            
+            if (updatedUser) {
+              response.writeHead(200, { 'Content-Type': 'application/json' });
+              response.write(JSON.stringify(updatedUser));
+              response.end();
+            } else {
+              response.writeHead(404, { 'Content-Type': 'application/json' });
+              response.write(`Can't find user with id: ${id}`);
+              response.end();
+            }
+        });
+      } else if(id && request.method === 'DELETE') {
+        const users = await Storage.apiDeleteUser(id).then((users: IUser[] | undefined) => users);
 
-  response.writeHead(404, { 'Content-Type': 'application/json' });
-  response.end(JSON.stringify({ message: 'Route not found' }));
-  response.end();
+        if(users) {
+          response.writeHead(200, { 'Content-Type': 'application/json' });
+          response.write(`Successfully finded and deleted user with id: ${id}`);
+          response.end();
+        } else {
+          response.writeHead(404, { 'Content-Type': 'application/json' });
+          response.write(`Can't find and delete user with id: ${id}`);
+          response.end();
+        }
+      }
+    } else  if (request.url === '/api/users' && request.method === 'GET') {
+      const users = await Storage.apiGetUsers().then((users: IUser[]) => users);
+
+      response.writeHead(200, { 'Content-Type': 'application/json' });
+      response.write(JSON.stringify(users));
+      response.end();
+    } else  if (request.url === '/api/users' && request.method === 'POST') {
+
+        let data = '';
+        
+        request.on('data', chunk => {
+          data += chunk.toString();
+        });
+        
+        request.on('end', async () => {
+          const params = JSON.parse(data);
+          const isValidData = dataChecker(params);
+
+          if(isValidData){
+
+            const userData: INewUser = {
+              username: params.username,
+              age: params.age,
+              hobbies: params.hobbies
+            };
+            
+            const newUser = await Storage.apiCreateUser(userData).then((user: IUser) => user);
+            
+            response.writeHead(201, { 'Content-Type': 'application/json' });
+            response.write(JSON.stringify(newUser));
+            response.end();
+          } else {
+            response.writeHead(400, { 'Content-Type': 'application/json' });
+            response.write('Sorry! Data is invalid. Note: username(sting), age(number), hobbies([] or [] of strings) are REQUIRED!)');
+            response.end();
+          }
+      });
+    } else {
+      response.writeHead(404, { 'Content-Type': 'application/json' });
+      response.write(JSON.stringify('Sorry! Looks like I can\'t resolve this route.'));
+      response.end();
+    }
+  } 
+  catch (error) {
+    if(error) {
+      response.writeHead(500, { 'Content-Type': 'application/json' });
+      response.write('Sorry! Something went wrong on our side.');
+      response.end();
+    }
+  }
 });
 
 server.listen(PORT, () => {
